f48cb2ba9b50cd7de2e0eb87971083f33de9d7da
ik-awais <mawaisqq@gmail.com>
2026-01-31 16:26:49 +0500

    Updated ab Task 2

--- Lab/Lab2/Q1.cpp
+++ Lab/Lab2/Q1.cpp
@@ -1,3 +1,12 @@
+// Use Double Pointer to solve it:
+// The Education Department selects one student for a merit-based scholarship from two
+// candidates. Their marks are stored in separate integer variables in main, and a pointer is
+// used to track the selected student. Write a C++ program with one function that takes
+// the addresses of both students’ marks and a double pointer to the selection pointer.
+// The function should compare the marks and update the pointer to the higher score.
+// Note: If both marks are equal, set the pointer to NULL. After returning to main, display
+// the selected marks or a message indicating that both students have equal marks.
+
 #include <iostream>
 using namespace std;
 
--- Lab/Lab2/Q2.cpp
+++ Lab/Lab2/Q2.cpp
@@ -1,25 +1,49 @@
+// Write a C++ program in which the user first enters the number of teachers. The program
+// should include a function calculate_training_score() that dynamically allocates an array
+// to store the scores, reads the scores from the user, and returns a pointer to the array back
+// to the main function. In the main function, a pointer array should be declared to store the
+// addresses of the individual scores.
+// // Another function, display_score(),should be used to display the scores by dereferencing
+// the pointers.
+// Note: Finally, all dynamically allocated memory must be properly deallocated before
+// the program ends.
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-int *calcTrainingScore(*arrSize);
+int *CalScore(int size);
+void DispScore(int *score, int size);
 
 int main(){
-    int size;
-    cout << "Enter number of teachers: ";
-    cin >> size;
-    int *scores[size];
-    
-
+    int num, *arrScore;
+    cout << "Enter number of Teachers in training: ";
+    cin >> num;
+    arrScore = CalScore(num);
+    DispScore(arrScore, num);
+    delete[] arrScore;
+    cout << "Memory Deallocated Successfully." << endl;
     return 0;
 }
 
-int *calcTrainingScore(int *arrSize){
-    
-    int *arr = new int [*arrSize];
-    cout << "Enter scores: ";
-    for (int i = 0; i < *arrSize; i++)
-        cin >> *arr;
-        arr++;
-    return arr-*arrSize;
+int *CalScore(int size){
+    int *score = new int[size], *temp = score;
+    for(int i = 0; i < size; i++)
+    {
+        cout << "Enter training score for Teacher ";
+        cout << i + 1 << ": ";
+        cin >> *score;
+        score++;
+    }
+    return temp;
+}
+
+void DispScore(int *score, int size){
+    cout << "Training scores: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Teacher" << i + 1 << ": ";
+        cout << *score << endl;
+        score++; 
+    }
 }
